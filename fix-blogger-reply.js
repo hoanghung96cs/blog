3d5029b7c687268536f0758351bc3b9071d7a93e
Hoàng Văn Hưng <hoanghung96cs@users.noreply.github.com>
2018-11-19 21:23:08 +0700

    Update fix-blogger-reply.js

--- fix-blogger-reply.js
+++ fix-blogger-reply.js
@@ -2,44 +2,31 @@
 
 <script src='//cdn.jsdelivr.net/npm/jquery@3/dist/jquery.min.js' type='text/javascript'></script>
 
-1. Blog1 version 1
+1. Tìm thẻ
 
-Tìm đến thẻ <b:includable id='threaded_comment_js' var='post'> và thay toàn bộ nội dung bên trong nó thành
+* Blog1 version 1 : tìm đến thẻ <b:includable id='threaded_comment_js' var='post'>
 
-<script type='text/javascript'>//<![CDATA[
-$(function() {
-  const t = $("#comment-editor-src").attr("href");
-  $(".comment-form").append('<a class="cancel-comment" style="font-family:Roboto,sans-serif;font-weight:700;font-size:12px;color:#ff0000;cursor:pointer;">CANCEL</a>'), $(".cancel-comment").hide(), $(".comment-reply").each(function() {
-    var e = $(this);
-    e.on("click", function(n) {
-      $(".comment-actions,.cancel-comment").show(), n.preventDefault();
-      let o = e.parents("li").attr("id").substr(1),
-        c = t.split("?");
-      $("#comment-editor")[0].src = c[0] + "?parentID=" + o + "&" + c[1], e.parent().hide(), e.parents("li").append($(".comment-form")[0])
-    })
-  }), $(".cancel-comment").on("click", function() {
-    $(".comment-actions").show(), $("#comment-editor")[0].src = t, $(".comments-content").before($(".comment-form")[0]), $(this).hide()
-  })
-});
-//]]></script>
+* Blog1 version 2 : tìm đến thẻ <b:includable id='threadedCommentJs' var='post'>
 
-2. Blog1 version 2
-
-Tìm đến thẻ <b:includable id='threadedCommentJs' var='post'> và thay toàn bộ nội dung bên trong nó thành
+2. Thay toàn bộ nội dung bên trong nó thành
 
 <script type='text/javascript'>//<![CDATA[
-$(function() {
-  const t = $("#comment-editor-src").attr("href");
-  $(".comment-form").append('<a class="cancel-comment" style="font-family:Roboto,sans-serif;font-weight:700;font-size:12px;color:#ff0000;cursor:pointer;">CANCEL</a>'), $(".cancel-comment").hide(), $("a[o='r']").each(function() {
-    var e = $(this);
-    e.on("click", function(n) {
-      $(".comment-actions,.cancel-comment").show(), n.preventDefault();
-      let o = e.parents(".comment-block").attr("id").substr(1),
-        c = t.split("?");
-      $("#comment-editor")[0].src = c[0] + "?parentID=" + o + "&" + c[1], e.parent().hide(), e.parents("li").append($(".comment-form")[0])
+$(window).on("load", function() {
+  function n(n, t) {
+    $(n).each(function() {
+      var n = $(this);
+      n.on("click", function(e) {
+        $(".comment-actions,.cancel-comment").show(), e.preventDefault();
+        let c = n.parents(t).attr("id").substr(1),
+          m = o.split("?");
+        $("#comment-editor")[0].src = m[0] + "?parentID=" + c + "&" + m[1], n.parent().hide(), n.parents("li").append($(".comment-form")[0])
+      })
     })
-  }), $(".cancel-comment").on("click", function() {
-    $(".comment-actions").show(), $("#comment-editor")[0].src = t, $(".comments-content").before($(".comment-form")[0]), $(this).hide()
+  }
+  $(".comment-form").append('<a class="cancel-comment" style="font-family:Roboto,sans-serif;font-weight:700;font-size:12px;color:#ff0000;cursor:pointer;">CANCEL</a>'), $(".cancel-comment").hide();
+  const o = $("#comment-editor-src").attr("href");
+  $(".comment-block[id]").length ? n("a[o='r']", ".comment-block") : n(".comment-reply", "li"), $(".cancel-comment").on("click", function() {
+    $(".comment-actions").show(), $("#comment-editor")[0].src = o, $(".comments-content").before($(".comment-form")[0]), $(this).hide()
   })
 });
 //]]></script>
